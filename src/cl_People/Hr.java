ce55aed1bedac1ba0741035e9531ac1fd55471e9
malcolm282 <troll@filizzola.fr>
2013-12-17 09:54:04 +0100

    Création de la classe HR

---
+++ src/cl_People/Hr.java
@@ -0,0 +1,16 @@
+package cl_People;
+
+import java.util.Date;
+
+public class Hr extends Employee {
+
+	public Hr(int _socialId, String _firstname, String _lastname,
+			String _phoneNumber, String _address, Date _birthDay,
+			Date _recrutingDay, Employee_container _container) {
+		super(_socialId, _firstname, _lastname, _phoneNumber, _address,
+				_birthDay, _recrutingDay, _container);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
